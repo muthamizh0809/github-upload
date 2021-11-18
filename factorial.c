b26e825481f64191676ff052f4785e7c72307757
MUTHAMIZHSELVAN.C <94265667+muthamizh0809@users.noreply.github.com>
2021-11-18 11:52:56 +0530

    Add files via upload
    done

---
+++ factorial.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+
+int factorial(int number) {
+  // TODO:- Your logic here
+  if (number < 0) return -1;
+  if (number == 0) return 1;
+  int result = 1;
+  for (int i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+// Note: Don't add any other printf statement which will impact test cases.
+// Also don't change main function, focus function implementation only
+// int main() {
+//   int num = 0, N = 0;
+
+//   scanf("%d", &N);
+//   for (int i = 0; i < N; i++) {
+//     scanf("%d", &num);
+//     printf("%d\n", factorial(num));
+//   }
+
+//   return 0;
+// }
